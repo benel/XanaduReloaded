81c2855d936308bc1e7d4f2aa07a8d89693fd926
Jonathan 'Slaals' Serra <jonathan.serra@utt.fr>
2016-06-13 02:17:09 +0200

    FIX : Transclusion includes 'deep' transclusions

--- lists/transclude.js
+++ lists/transclude.js
@@ -6,21 +6,62 @@
  * @param {object} req - Request Object. http://docs.couchdb.org/en/latest/json-structure.html#request-object
  **/
 function(head, req) {
+  var getTransclusions = function(doc, transDoc, rgx1, rgx2) {
+    var data = doc.data;
+    doc.data = data.replace(rgx1, '[' + transDoc.data + ']()'); // Transdelivery
+    doc.data = data.replace(rgx2, '[' + transDoc.data + ']' + '(' + transDoc._id + ')'); // Transquote
+    return doc;
+  }
+  var getTransdeliveryRgx = function(transDoc) {
+    return new RegExp('{{ ' + transDoc._id + ' }}', 'g');
+  }
+  var getTransquoteRgx = function(transDoc) {
+    return new RegExp('{{ =?' + transDoc._id + ' }}', 'g');
+  }
+  var findDoc = function(doc) {
+
+  }
   provides('json', function() {
-    var data = [];
     var json = [];
+    var pushLaterList = [];
+    var docs = [];
     var row, doc;
+    var transcludeRgx = /\{\{ =?[\w\d]* \}\}/g;
     while(row = getRow()) {
+      // Get document which transcludes other documents
       if(row.key[1] === 0) {
-        if(doc && data[doc._id]) json.push({ '_id': doc._id, 'data': data[doc._id] });
+        if(doc) json.push(doc);
         doc = row.doc;
-        data[doc._id] = doc.data;
         continue;
       }
+      // Get transcluded documents data in order to transclude them in the "parent" document
       var transDoc = row.doc;
-      var transPattern = new RegExp('{{ ' + transDoc._id + ' }}', 'g');
-      data[doc._id] = data[doc._id].replace(transPattern, transDoc.data);
+
+      if(transcludeRgx.test(transDoc.data)) {
+        pushLaterList[doc._id] = {};
+        if(!pushLaterList[doc._id].transcludes) pushLaterList[doc._id].transcludes = [];
+        pushLaterList[doc._id].transcludes[transDoc._id] = transDoc;
+        pushLaterList[doc._id].doc = doc;
+      } else {
+        doc = getTransclusions(doc, transDoc, getTransdeliveryRgx(transDoc), getTransquoteRgx(transDoc));
+        docs[doc._id] = doc;
+      }
     }
+    // Push last document
+    json.push(doc);
+
+    for(var laterDocId in pushLaterList) {
+      var laterTransDocs = pushLaterList[laterDocId].transcludes;
+      var laterDoc = pushLaterList[laterDocId].doc;
+      for(var transDocId in laterTransDocs) { // recursive transclusion
+        var laterTransDoc = laterTransDocs[transDocId];
+        laterDoc = getTransclusions(json[json.indexOf(laterDoc)], docs[laterTransDoc._id], getTransdeliveryRgx(laterTransDoc), getTransquoteRgx(laterTransDoc));
+        json[json.indexOf(laterDoc)] = laterDoc;
+
+        docs[doc._id] = laterDoc;
+      }
+    }
+
     send(JSON.stringify(json));
   });
 }
--- views/transclude/map.js
+++ views/transclude/map.js
@@ -1,9 +1,11 @@
 function (doc) {
+  // Query one document : ?startkey=["doc id"]&endkey=["doc id", {}]
   var index = 1;
-  var pattern = /\{\{ [\w\d]* \}\}/g;
+  var pattern = /\{\{ =?[\w\d]* \}\}/g;
   var matches = pattern.exec(doc.data);
   while(matches) {
-    var transDocId = matches[0].replace(/\{*\}*/g, '').trim();
+    var transDocId = matches[0].replace(/[\{\{]*[\}\}]*/g, '').trim();
+    transDocId = transDocId.replace(/^=/g, '');
     emit([doc._id, index], { _id: transDocId });
     matches = pattern.exec(doc.data);
     ++index;
